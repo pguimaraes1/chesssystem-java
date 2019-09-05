da8308bd0748585e3cf0a11bb51346c23dd42088
Phellipe <phellipe.sander1@gmail.com>
2019-09-05 16:10:02 -0300

    Queen

---
+++ src/Chess/pieces/Queen.java
@@ -0,0 +1,105 @@
+package chess.pieces;
+
+import boradgame.Board;
+import boradgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "Q";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position position = new Position(0, 0);
+
+		// above
+		position.setValues(this.position.getRow() - 1, this.position.getColumn());
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() - 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// left
+		position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() - 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// right
+		position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setColumn(position.getColumn() + 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// below
+		position.setValues(this.position.getRow() + 1, this.position.getColumn());
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setRow(position.getRow() + 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// nw
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setValues(position.getRow() -1, position.getColumn() - 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// ne
+		position.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setValues(position.getRow() - 1, position.getColumn() + 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+
+		// sw
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setValues(position.getRow() + 1, position.getColumn() + 1);
+		}
+		if (getBoard().positionExistis(position) && isThereOpponentPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+		}
+		
+		// se
+		position.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		while (getBoard().positionExistis(position) && !getBoard().thereIsAPiece(position)) {
+			mat[position.getRow()][position.getColumn()] = true;
+			position.setValues(position.getRow() + 1, position.getColumn() - 1);
+		}
+
+		return mat;
+	}
+
+}
